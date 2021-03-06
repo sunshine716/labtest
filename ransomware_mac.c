83f74fc0a1a1ceae3a1c929644d55cf22ad11f78
Weiyang Zhang <sunnylocus@email.arizona.edu>
2021-03-05 23:16:29 -0700

    update

--- ransomware_mac.c
+++ ransomware_mac.c
@@ -61,16 +61,6 @@ const int create_large_file()
 	return 0;
 }
 
-void format_time(char *output){
-    time_t rawtime;
-    struct tm * timeinfo;
-
-    time ( &rawtime );
-    timeinfo = localtime ( &rawtime );
-
-    sprintf(output, "[%d %d %d %d:%d:%d]",timeinfo->tm_mday, timeinfo->tm_mon + 1, timeinfo->tm_year + 1900, timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
-}
-
 
 int main(int argc, char const *argv[])
 {
